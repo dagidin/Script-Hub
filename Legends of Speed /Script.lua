320b6df6cdf1172ddbcd21fcfc8736275afb956a
dag <125142970+dagidin@users.noreply.github.com>
2023-10-25 10:42:49 -0600

    Update Script.lua
    Removed the Function because that's gay

--- Legends of Speed /Script.lua
+++ Legends of Speed /Script.lua
@@ -7,47 +7,38 @@ getgenv().AutoRebirth = true
 
 local Player = game:GetService("Players").LocalPlayer
 
-local function CollectOrbsFunc()
-    spawn(function()
-        local PlayerHead = Player.Character:FindFirstChild("Head")
-        while task.wait() do
-            if not getgenv().CollectOrbs then break end
-            for i, OrbTouch in pairs(game:GetService("Workspace").orbFolder:GetDescendants()) do
-                if OrbTouch.Name == "TouchInterest" and OrbTouch.Parent then
-                    firetouchinterest(PlayerHead, OrbTouch.Parent, 1)
-                    task.wait(0.1)
-                    firetouchinterest(PlayerHead, OrbTouch.Parent, 0)
-                end
+spawn(function()
+    local PlayerHead = Player.Character:FindFirstChild("Head")
+    while task.wait() do
+        if not getgenv().CollectOrbs then break end
+        for i, OrbTouch in pairs(game:GetService("Workspace").orbFolder:GetDescendants()) do
+            if OrbTouch.Name == "TouchInterest" and OrbTouch.Parent then
+                firetouchinterest(PlayerHead, OrbTouch.Parent, 1)
+                task.wait(0.1)
+                firetouchinterest(PlayerHead, OrbTouch.Parent, 0)
             end
         end
-    end)
-end
+    end
+end)
 
-local function CollectHoopsFunc()
-    spawn(function()
-        local PlayerHead = Player.Character:FindFirstChild("Head")
-        while task.wait() do
-            if not getgenv().CollectHoops then break end
-            for i, HoopTouch in pairs(game:GetService("Workspace").Hoops:GetDescendants()) do
-                if HoopTouch.Name == "TouchInterest" and HoopTouch.Parent then
-                    firetouchinterest(PlayerHead, HoopTouch.Parent, 0)
-                    task.wait(0.1)
-                    firetouchinterest(PlayerHead, HoopTouch.Parent, 1)
-                end
+spawn(function()
+    local PlayerHead = Player.Character:FindFirstChild("Head")
+    while task.wait() do
+        if not getgenv().CollectHoops then break end
+        for i, HoopTouch in pairs(game:GetService("Workspace").Hoops:GetDescendants()) do
+            if HoopTouch.Name == "TouchInterest" and HoopTouch.Parent then
+                firetouchinterest(PlayerHead, HoopTouch.Parent, 0)
+                task.wait(0.1)
+                firetouchinterest(PlayerHead, HoopTouch.Parent, 1)
             end
         end
-    end)
-end
+    end
+end)
 
-local function AutoRebirthFunc()
-    spawn(function()
-        while task.wait(5) do
-            if not getgenv().AutoRebirth then break end
-            game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer("rebirthRequest")
-        end
-    end)
-end
 
-CollectOrbsFunc()
-CollectHoopsFunc()
-AutoRebirthFunc()
+spawn(function()
+    while task.wait(5) do
+        if not getgenv().AutoRebirth then break end
+        game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer("rebirthRequest")
+    end
+end)
