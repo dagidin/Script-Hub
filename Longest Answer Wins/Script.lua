e385996e0ae3a431035c3135155530889ae9f4c4
dag <125142970+dagidin@users.noreply.github.com>
2023-10-24 23:55:41 -0600

    Create Script.lua

---
+++ Longest Answer Wins/Script.lua
@@ -0,0 +1,30 @@
+repeat task.wait() until game:IsLoaded()
+
+-- if you're snooping around my code, feel free to change stuff up! :^)
+getgenv().AutoAnswer = true
+
+local RepStorage = game:GetService("ReplicatedStorage")
+local StarterGui = game:GetService("StarterGui")
+
+local Hint = RepStorage:FindFirstChild("HintAnswer")
+local Submit = RepStorage:FindFirstChild("SubmittedAnswer")
+
+local function Notify(Txt)
+    StarterGui:SetCore("SendNotification", {
+        Title = "Answer",
+        Text = Txt
+    })
+end
+
+local function AnswerQuestion()
+    local Correct = Hint.Value
+    local Length = #Correct
+    
+    task.wait()
+    if Correct ~= "" then Notify(Correct) end
+
+    task.wait(math.random(20, 30) / 10 + Length / 5)
+    Submit:FireServer(Correct, Length)
+end
+
+Hint:GetPropertyChangedSignal("Value"):Connect(AnswerQuestion)
