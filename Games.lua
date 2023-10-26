18c8d446a309780ac4851df059815e57daacf00e
dag <125142970+dagidin@users.noreply.github.com>
2023-10-26 12:06:57 -0600

    Create Games.lua

---
+++ Games.lua
@@ -0,0 +1,7 @@
+getgenv().Games = {
+  {"Evade", 9872472334, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Evade/Script.lua"},
+  {"The Spinner", 10228777202, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/The%20Spinner/Script.lua"},
+  {"Legends of Speed", 3101667897, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Legends%20of%20Speed%20/Script.lua"},
+  {"Longest Answer Wins", 9992339729, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Longest%20Answer%20Wins/Script.lua"},
+  {"Build a Boat for Treasure", 537413528, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Build%20a%20Boat%20for%20Treasure/Script.lua"}
+}
