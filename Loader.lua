b1518ce1e0e8577c54b94cbfb82be0d697817a50
dag <125142970+dagidin@users.noreply.github.com>
2023-10-26 12:07:51 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,4 @@
-local Games = {
-    {"Legends of Speed", 3101667897, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Legends%20of%20Speed%20/Script.lua"},
-    {"Build a Boat for Treasure", 537413528, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Build%20a%20Boat%20for%20Treasure/Script.lua"},
-    {"The Spinner" , 10228777202, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/The%20Spinner/Script.lua"},
-    {"Longest Answer Wins", 9992339729, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Longest%20Answer%20Wins/Script.lua"},
-    {"Evade", 9872472334, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Evade/Script.lua"}
-}
+local Games = game:HttpGet("https://raw.githubusercontent.com/dagidin/Script-Hub/main/Games.lua")
 
 for i, Game in ipairs(Games) do
     if game.PlaceId == Game[2] then
