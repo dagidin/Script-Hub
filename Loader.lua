3c4e25c665d25af55ac8f4aeb86142047e459efa
dag <125142970+dagidin@users.noreply.github.com>
2023-10-26 12:02:13 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,8 @@ local Games = {
     {"Legends of Speed", 3101667897, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Legends%20of%20Speed%20/Script.lua"},
     {"Build a Boat for Treasure", 537413528, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Build%20a%20Boat%20for%20Treasure/Script.lua"},
     {"The Spinner" , 10228777202, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/The%20Spinner/Script.lua"},
-    {"Longest Answer Wins", 9992339729, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Longest%20Answer%20Wins/Script.lua"}
+    {"Longest Answer Wins", 9992339729, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Longest%20Answer%20Wins/Script.lua"},
+    {"Evade", 9872472334, "https://raw.githubusercontent.com/dagidin/Script-Hub/main/Evade/Script.lua"}
 }
 
 for i, Game in ipairs(Games) do
